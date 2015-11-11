f86edf706358e89c59e9eb94908d5c2876924372
Marie Porciuncula <marieporciuncula@Maries-MacBook-Air.local>
2015-11-10 22:43:19 -0800

    commit

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,25 +18,26 @@ angular.module('angularJsApp')
     };
 
     $scope.saveCity = function(city){
-        var hikeData = {
-            'name': city.name,
-            'id': city.id
-        };
         if (!$localStorage.savedCities){
-            $localStorage.savedCities = [hikeData];
+            $localStorage.savedCities = [];
+            $localStorage.savedCities.push(city);
         } else {
             // We have already saved some cities. 
             // Check to make sure we haven't already saved the current city.
+
             var save = true; // Initialize the save decision variable.
+
             // Use this loop to check if we've already saved the city.
+
             for (var i=0; i < $localStorage.savedCities.length; i++){
-                if ($localStorage.savedCities[i].id == cityData.id) {
+                if ($localStorage.savedCities[i].id === city.id) {
                     // This is a duplicate, so don't save (variable set to false).
                     save = false;
                 }
             }
-            if (save==true){
-                $localStorage.savedCities.push(hikeData);
+
+            if (save){
+                $localStorage.savedCities.push(city);
             } else {
                 console.log('city already saved');
             }
--- app/scripts/services/directions.js
+++ app/scripts/services/directions.js
@@ -1,18 +1,18 @@
-'use strict';
+// 'use strict';
 
-/**
- * @ngdoc service
- * @name angularJsApp.directions
- * @description
- * # directions
- * Factory in the angularJsApp.
- */
-angular.module('angularJsApp')
-  .factory('DirectionsCtrl', function ($Scope, $routeParams, directions, $localStorage) {
-         $scope.city_eq = $routeParams.city_eq;
+// /**
+//  * @ngdoc service
+//  * @name angularJsApp.directions
+//  * @description
+//  * # directions
+//  * Factory in the angularJsApp.
+//  */
+// angular.module('angularJsApp')
+//   .factory('DirectionsCtrl', function ($Scope, $routeParams, directions, $localStorage) {
+//          $scope.city_eq = $routeParams.city_eq;
 
-    $scope.hikeData = hikeSearch.query({
-        city_eq: $routeParams.city_eq  
-    });
-  });
+//     $scope.hikeData = hikeSearch.query({
+//         city_eq: $routeParams.city_eq  
+//     });
+//   });
 
