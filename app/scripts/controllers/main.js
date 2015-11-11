f9b7fd6c542c9ecdcd389081b97ca81d31f7edf5
Marie Porciuncula <marieporciuncula@maries-air.seattleu.edu>
2015-11-10 20:44:52 -0800

    fixed broken main view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,31 +17,32 @@ angular.module('angularJsApp')
         });
     };
 
+    $scope.saveCity = function(city){
+        var hikeData = {
+            'name': city.name,
+            'id': city.id
+        };
+        if (!$localStorage.savedCities){
+            $localStorage.savedCities = [hikeData];
+        } else {
+            // We have already saved some cities. 
+            // Check to make sure we haven't already saved the current city.
+            var save = true; // Initialize the save decision variable.
+            // Use this loop to check if we've already saved the city.
+            for (var i=0; i < $localStorage.savedCities.length; i++){
+                if ($localStorage.savedCities[i].id == cityData.id) {
+                    // This is a duplicate, so don't save (variable set to false).
+                    save = false;
+                }
+            }
+            if (save==true){
+                $localStorage.savedCities.push(hikeData);
+            } else {
+                console.log('city already saved');
+            }
+        }
+    };
+
   });
 
 
-$scope.saveCity = function(city){
-    var hikeData = {
-        'name': city.name,
-        'id': city.id
-    };
-    if (!$localStorage.savedCities){
-        $localStorage.savedCities = [hikeData];
-    } else {
-        // We have already saved some cities. 
-        // Check to make sure we haven't already saved the current city.
-        var save = true; // Initialize the save decision variable.
-        // Use this loop to check if we've already saved the city.
-        for (var i=0; i < $localStorage.savedCities.length; i++){
-            if ($localStorage.savedCities[i].id == cityData.id) {
-                // This is a duplicate, so don't save (variable set to false).
-                save = false;
-            }
-        }
-        if (save==true){
-            $localStorage.savedCities.push(hikeData);
-        } else {
-            console.log('city already saved');
-        }
-    }
-};
