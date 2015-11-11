a1c130abf7a98bae33fac178a715aee8f95177a0
Marie Porciuncula <marieporciuncula@maries-air.seattleu.edu>
2015-11-10 19:26:27 -0800

    commit

--- app/scripts/services/directions.js
+++ app/scripts/services/directions.js
@@ -16,26 +16,3 @@ angular.module('angularJsApp')
     });
   });
 
-
-    // Service logic
-    // ...
-
-
-
-    // Public API here
-
-    return $resource(
-      'https://outdoor-data-api.herokuapp.com/api.json?api_key=3d915e7fea908a3df09ece5462427d44&city_eq=:city_eq', {},
-      {
-        find: {
-          method: 'JSONP',
-          params: {
-            city_eq: '',
-            callback: 'JSON_CALLBACK'
-          }
-        }
-      }
-    });
-
-  });
-
